86fe06a4f4e6a335b9599cb45bf6900ad8ec94de
jayd <j@w3reality.com>
2019-10-18 11:33:58 +0700

    Add Utils.rotateTurfObject()

--- src/Utils.js
+++ src/Utils.js
@@ -2,6 +2,7 @@ import * as THREE_ES6 from 'three';
 const THREE = window.THREE ? window.THREE : THREE_ES6;
 
 import turfTransformTranslate from '@turf/transform-translate';
+import turfTransformRotate from '@turf/transform-rotate';
 
 // TODO doc
 class Utils {
@@ -12,6 +13,12 @@ class Utils {
                 units: 'meters',
                 zTranslation: dz / unitsPerMeter,
                 mutate: mutate, // "significant performance increase if true" per doc
+            });
+    }
+    static rotateTurfObject(turfObj, deg, pivotLatlng, mutate=true) {
+        return turfTransformRotate(turfObj, deg, {
+                pivot: [pivotLatlng[1], pivotLatlng[0]],
+                mutate: mutate,
             });
     }
 
