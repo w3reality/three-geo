3d6efc9e60678fb6b548c8eb8d380800934daa06
jayd <j@w3reality.com>
2019-10-19 10:44:41 +0700

    Throw a warning if necessary on checking the token

--- examples/heightmaps/index.js
+++ examples/heightmaps/index.js
@@ -66,72 +66,74 @@ const demToObjects = (demUri, demTile, proj) => {
 const $msg = $('#msg');
 
 if (tgeo.tokenMapbox.startsWith('****')) {
-    const warning = 'Please set your Mapbox API token in ThreeGeo constructor.';
+    const warning = 'Please set your Mapbox API token in the ThreeGeo constructor.';
     // alert(warning);
     $msg.append(`<div>${warning}</div>`);
-} else {
-    // params: [lat, lng], terrain's radius (km), zoom resolution, callbacks
-    // Beware the value of radius; radius > 5.0 (km) could trigger huge number of tile API calls!!
-    const origin = [46.5763, 7.9904];
-    const radius = 5.0;
-    const { proj, bbox, unitsPerMeter } = tgeo.getProjection(origin, radius);
-    // console.log('proj:', proj);
-    // console.log('unitsPerMeter:', unitsPerMeter);
-
-    const srcDemUris = {};
-
-    $msg.empty();
-    $msg.append(`<div>---- ROI ----</div>`);
-    $msg.append(`<div>lat lng: (${origin[0]}, ${origin[1]})</div>`);
-    $msg.append(`<div>radius: ${radius} [km]</div>`);
-    $msg.append(`<div>units per km: ${unitsPerMeter * 1000}</div>`);
-    $msg.append(`<div>bbox (w, s, e, n): (${bbox.map(q => q.toFixed(4)).join(', ')})</div>`);
-    $msg.append(`<div>---- Terrain Composition ----</div>`);
-
-    tgeo.getTerrain(origin, radius, 12, {
-        onRgbDem: (meshes) => {
-            meshes.forEach((mesh) => {
-                console.log('rgb DEM mesh:', mesh);
-                group.add(mesh);
-                console.log('userData:', mesh.userData);
-
-                //======== how to access the post-processed heightmap
-                const array = mesh.geometry.attributes.position.array;
-                // console.log('array:', array);
-                console.log('array.length:', array.length); // 3x128x128 (+ deltaSeams)
-
-                //======== how to visualize constituent tiles of the terrain
-                const tile = mesh.userData.threeGeo.tile;
-                const { obj, offset } = ThreeGeo.Utils.bboxToWireframe(
-                    ThreeGeo.Utils.tileToBbox(tile), proj, {offsetZ: - 0.05});
-
-                const sp = createTextSprite(`${tile.join('-')}`, '#0ff');
-                sp.position.set(offset[0], offset[1], offset[2] + 0.05);
-                group.add(obj, sp);
-
-                //======== how to access src DEM being used (grand-parental tile)
-                // ref - https://www.mapbox.com/help/access-elevation-data/#mapbox-terrain-rgb
-                const srcDem = mesh.userData.threeGeo.srcDem;
-                const srcDemUri = `${srcDem.uri}${tgeo.tokenMapbox}`;
-                const srcDemTile = srcDem.tile;
-                // console.log('srcDemUri:', srcDemUri);
-
-                if (! srcDemUris[srcDemUri]) {
-                    // console.log('adding:', srcDemUri);
-                    srcDemUris[srcDemUri] = true;
-
-                    const {wireframe, plane, sprite} = demToObjects(srcDemUri, srcDemTile, proj);
-                    group.add(wireframe, plane, sprite);
-                }
-
-                $msg.append(`<div><span style="color: #00ffffff;">tile ${tile.join('-')}</span> using <span style="color: #ff00ffff";>DEM ${srcDemTile.join('-')}</span></div>`);
-            });
-            render();
-        },
-        onSatelliteMat: (mesh) => {
-            mesh.material.wireframe = true;
-            // mesh.material.side = THREE.DoubleSide;
-            render();
-        },
-    });
+    alert(warning);
+    throw warning;
 }
+
+// params: [lat, lng], terrain's radius (km), zoom resolution, callbacks
+// Beware the value of radius; radius > 5.0 (km) could trigger huge number of tile API calls!!
+const origin = [46.5763, 7.9904];
+const radius = 5.0;
+const { proj, bbox, unitsPerMeter } = tgeo.getProjection(origin, radius);
+// console.log('proj:', proj);
+// console.log('unitsPerMeter:', unitsPerMeter);
+
+const srcDemUris = {};
+
+$msg.empty();
+$msg.append(`<div>---- ROI ----</div>`);
+$msg.append(`<div>lat lng: (${origin[0]}, ${origin[1]})</div>`);
+$msg.append(`<div>radius: ${radius} [km]</div>`);
+$msg.append(`<div>units per km: ${unitsPerMeter * 1000}</div>`);
+$msg.append(`<div>bbox (w, s, e, n): (${bbox.map(q => q.toFixed(4)).join(', ')})</div>`);
+$msg.append(`<div>---- Terrain Composition ----</div>`);
+
+tgeo.getTerrain(origin, radius, 12, {
+    onRgbDem: (meshes) => {
+        meshes.forEach((mesh) => {
+            console.log('rgb DEM mesh:', mesh);
+            group.add(mesh);
+            console.log('userData:', mesh.userData);
+
+            //======== how to access the post-processed heightmap
+            const array = mesh.geometry.attributes.position.array;
+            // console.log('array:', array);
+            console.log('array.length:', array.length); // 3x128x128 (+ deltaSeams)
+
+            //======== how to visualize constituent tiles of the terrain
+            const tile = mesh.userData.threeGeo.tile;
+            const { obj, offset } = ThreeGeo.Utils.bboxToWireframe(
+                ThreeGeo.Utils.tileToBbox(tile), proj, {offsetZ: - 0.05});
+
+            const sp = createTextSprite(`${tile.join('-')}`, '#0ff');
+            sp.position.set(offset[0], offset[1], offset[2] + 0.05);
+            group.add(obj, sp);
+
+            //======== how to access src DEM being used (grand-parental tile)
+            // ref - https://www.mapbox.com/help/access-elevation-data/#mapbox-terrain-rgb
+            const srcDem = mesh.userData.threeGeo.srcDem;
+            const srcDemUri = `${srcDem.uri}${tgeo.tokenMapbox}`;
+            const srcDemTile = srcDem.tile;
+            // console.log('srcDemUri:', srcDemUri);
+
+            if (! srcDemUris[srcDemUri]) {
+                // console.log('adding:', srcDemUri);
+                srcDemUris[srcDemUri] = true;
+
+                const {wireframe, plane, sprite} = demToObjects(srcDemUri, srcDemTile, proj);
+                group.add(wireframe, plane, sprite);
+            }
+
+            $msg.append(`<div><span style="color: #00ffffff;">tile ${tile.join('-')}</span> using <span style="color: #ff00ffff";>DEM ${srcDemTile.join('-')}</span></div>`);
+        });
+        render();
+    },
+    onSatelliteMat: (mesh) => {
+        mesh.material.wireframe = true;
+        // mesh.material.side = THREE.DoubleSide;
+        render();
+    },
+});
