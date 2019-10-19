7856357230f943044bf11c3b0c9381964a82b6f4
jayd <j@w3reality.com>
2019-10-19 13:29:31 +0700

    Reorganize the geo-viewer app in an OOP manner

--- examples/geo-viewer/src/index.js
+++ examples/geo-viewer/src/index.js
@@ -5,173 +5,149 @@ import env from './envs-ignore/env-dev.js';
 import GuiHelper from './gui-helper.js';
 import Viewer from './viewer.js';
 
-//
+class App extends Threelet {
+    // override
+    onCreate(params) {
+        this.camera.position.set(0, 0, 1.5);
+        this.camera.up.set(0, 0, 1); // The up vector is along +z for this app
 
-const threelet = new Threelet({
+        const stats = this.setup('mod-stats', window.Stats, {panelType: 1});
+        const viewer = new Viewer(env, this.canvas, this.camera, this.renderer);
+
+        this.render = () => { // override
+            stats.update();
+            this.resizeCanvas();
+            viewer.render();
+            viewer.showMsg(this.camera);
+            viewer.plotCamInMap(this.camera);
+        };
+        this.setup('mod-controls', THREE.OrbitControls);
+
+        const guiData = App.createGuiData();
+        viewer.setGuiHelper(
+            App.createGuiHelper(env, guiData, viewer, this.render));
+
+        // viewer.closeGui();
+        viewer.toggleMap(guiData.leaflet);
+        viewer.showMsg(this.camera);
+        viewer.plotCamInMap(this.camera);
+        viewer.showMsgTerrain();
+
+        this.setupMouseInterface(viewer);
+        this._appData = { stats, viewer, guiData };
+    }
+
+    setupMouseInterface(viewer) {
+        this.on('mouse-move', (mx, my) => viewer.pick(mx, my));
+        this.on('mouse-click', (mx, my) => viewer.updateMeasure(mx, my));
+        this.on('mouse-click-right', (mx, my) => viewer.updateOrbit(mx, my));
+    }
+
+    static createGuiData() {
+        const query = Viewer.parseQuery();
+        return { // with defaults
+            vis: query.mode,
+            grids: true,
+            autoOrbit: false,
+            vrLaser: false,
+            //----
+            loc: query.title ? query.title.replace('_', ' ') : "",
+            leaflet: true,
+        };
+    }
+    static createAnimToggler(render) {
+        let stopAnim = true;
+        const animate = () => {
+            if (stopAnim) {
+                console.log('animate(): stopping');
+                return;
+            }
+            requestAnimationFrame(animate);
+            render();
+        };
+
+        return (tf) => {
+            if (tf) {
+                stopAnim = false;
+                animate();
+            } else {
+                stopAnim = true;
+            }
+        };
+    }
+    static createGuiHelper(env, guiData, viewer, render) {
+        const animToggler = this.createAnimToggler(render); // a closure
+        const guiHelper = new GuiHelper(env, guiData, {
+            onCapture: () => {
+                viewer.capture();
+            },
+            onChangeGrids: (value) => {
+                viewer.toggleGrids(value);
+            },
+            onChangeAutoOrbit: (value) => {
+                viewer.toggleOrbiting(value);
+                if (value) {
+                    if (! viewer.hasOrbit()) {
+                        viewer.setOrbitDefault();
+                    }
+                    console.log('starting anim...');
+                    animToggler(true);
+                } else {
+                    console.log('stopping anim...');
+                    animToggler(false);
+                }
+            },
+            onChangeVis: (value) => {
+                console.log('vis:', value);
+                if (value === 'Contours') {
+                    viewer.loadVectorDem(() => {
+                        viewer.updateMode(value);
+                        render();
+                    });
+                } else {
+                    viewer.loadRgbDem(() => {
+                        viewer.updateMode(value);
+                        render();
+                    });
+                }
+            },
+            onChangeVrLaser: (value) => {
+                viewer.toggleVrLaser(value);
+            },
+            onChangeLeaflet: (value) => {
+                viewer.toggleMap(value);
+            },
+            onChangeLoc: (value, locations) => {
+                if (value === "(none)") { // dummy case
+                    return;
+                }
+
+                if (value in locations) {
+                    let title = value.replace(' ', '_');
+                    let ll = locations[value];
+                    viewer.reloadPageWithLocation(ll, title);
+                }
+            },
+        });
+        guiHelper.setDefaults({
+            isDev: () => {},
+            vis: guiData.vis,
+            capture: () => {},
+            grids: guiData.grids,
+            //----
+            autoOrbit: guiData.autoOrbit,
+            vrLaser: guiData.vrLaser,
+            reset: () => {},
+            //----
+            loc: guiData.loc,
+            leaflet: guiData.leaflet,
+            sourceCode: () => {},
+        });
+        return guiHelper;
+    }
+}
+
+const app = new App({
     canvas: document.getElementById("canvas"),
 });
-const { scene, canvas, camera, renderer } = threelet;
-camera.position.set(0, 0, 1.5);
-camera.up.set(0, 0, 1); // important for OrbitControls
-
-const stats = threelet.setup('mod-stats', window.Stats, {panelType: 1});
-const viewer = new Viewer(env, canvas, camera, renderer);
-console.log('viewer:', viewer);
-
-const render = () => {
-    stats.update();
-    threelet.resizeCanvas();
-    viewer.render();
-    viewer.showMsg(camera);
-    viewer.plotCamInMap(camera);
-};
-threelet.render = render; // override
-const controls = threelet.setup('mod-controls', THREE.OrbitControls);
-
-const group = new THREE.Group();
-group.rotation.x = - Math.PI/2;
-scene.add(group);
-
-//
-
-let _stopAnim = true;
-const _animate = () => {
-    if (_stopAnim) {
-        console.log('_animate(): stopping');
-        return;
-    }
-    requestAnimationFrame(_animate);
-    render();
-};
-const toggleAnimation = (tf) => {
-    if (tf) {
-        _stopAnim = false;
-        _animate();
-    } else {
-        _stopAnim = true;
-    }
-};
-
-//
-
-const query = Viewer.parseQuery();
-const guiData = { // with defaults
-    vis: query.mode,
-    grids: true,
-    autoOrbit: false,
-    vrLaser: false,
-    //----
-    loc: query.title ? query.title.replace('_', ' ') : "",
-    leaflet: true,
-};
-const guiHelper = new GuiHelper(env, guiData, {
-    onCapture: () => {
-        viewer.capture();
-    },
-    onChangeGrids: (value) => {
-        viewer.toggleGrids(value);
-    },
-    onChangeAutoOrbit: (value) => {
-        viewer.toggleOrbiting(value);
-        if (value) {
-            if (! viewer.hasOrbit()) {
-                viewer.setOrbitDefault();
-            }
-            console.log('starting anim...');
-            toggleAnimation(true);
-        } else {
-            console.log('stopping anim...');
-            toggleAnimation(false);
-        }
-    },
-    onChangeVis: (value) => {
-        console.log('vis:', value);
-        if (value === 'Contours') {
-            viewer.loadVectorDem(() => {
-                viewer.updateMode(value);
-                render();
-            });
-        } else {
-            viewer.loadRgbDem(() => {
-                viewer.updateMode(value);
-                render();
-            });
-        }
-    },
-    onChangeVrLaser: (value) => {
-        viewer.toggleVrLaser(value);
-    },
-    onChangeLeaflet: (value) => {
-        viewer.toggleMap(value);
-    },
-    onChangeLoc: (value, locations) => {
-        if (value === "(none)") { // dummy case
-            return;
-        }
-
-        if (value in locations) {
-            let title = value.replace(' ', '_');
-            let ll = locations[value];
-            viewer.reloadPageWithLocation(ll, title);
-        }
-    },
-});
-guiHelper.setDefaults({
-    isDev: () => {},
-    vis: guiData.vis,
-    capture: () => {},
-    grids: guiData.grids,
-    //----
-    autoOrbit: guiData.autoOrbit,
-    vrLaser: guiData.vrLaser,
-    reset: () => {},
-    //----
-    loc: guiData.loc,
-    leaflet: guiData.leaflet,
-    sourceCode: () => {},
-});
-// guiHelper.gui.close();
-viewer.setGuiHelper(guiHelper);
-
-const getMouseCoords = e => {
-    // https://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element/18053642#18053642
-    let rect = canvas.getBoundingClientRect();
-    let mx = e.clientX - rect.left;
-    let my = e.clientY - rect.top;
-    // console.log('getMouseCoords():', mx, my, canvas.width, canvas.height);
-    return [mx, my];
-};
-
-// https://stackoverflow.com/questions/6042202/how-to-distinguish-mouse-click-and-drag
-let isDragging = false;
-renderer.domElement.addEventListener("mousedown", e => {
-    isDragging = false;
-}, false);
-renderer.domElement.addEventListener("mousemove", e => {
-    isDragging = true;
-    let coords = getMouseCoords(e);
-    viewer.pick(coords[0], coords[1]);
-}, false);
-renderer.domElement.addEventListener("mouseup", e => {
-    // console.log('e:', e);
-    if (isDragging) {
-        console.log("mouseup: drag");
-        // nop
-    } else {
-        console.log("mouseup: click");
-        let coords = getMouseCoords(e);
-        if (e.button === 0) {
-            viewer.updateMeasure(coords[0], coords[1]);
-        } else if (e.button === 2) {
-            viewer.updateOrbit(coords[0], coords[1]);
-        }
-    }
-}, false);
-
-// main
-
-render(); // first time
-viewer.toggleMap(guiData.leaflet);
-viewer.showMsg(camera);
-viewer.plotCamInMap(camera);
-viewer.showMsgTerrain();
+app.render(); // first time
