1c3f1d481d887434c9f3743e7c5df9f8d0d6b18a
j-devel <j@w3reality.com>
2020-01-31 12:10:07 +0700

    Bump to 1.3.3dev

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const __version = "1.3.1";
+const __version = "1.3.3dev";
 
 // if THREE is global (via script-tag loading), use that THREE to prevent
 // conflicts with ES6 version. (Line objects become broken, otherwise...)
