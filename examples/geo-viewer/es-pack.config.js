7cf0cb2b725ac18181ed44d2f9af7b729b0bf0e6
j-devel <j@w3reality.com>
2022-03-28 11:46:39 +0700

    geo-viewer: Fix "ReferenceError: Buffer is not defined"

--- examples/geo-viewer/es-pack.config.js
+++ examples/geo-viewer/es-pack.config.js
@@ -2,9 +2,14 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = {
-    onBundle: (webpackConfig) => {
+    onBundle: (webpackConfig, webpack) => {
         webpackConfig.externals = {'three': 'THREE'}; // this makes sure `three.module.js` is not pulled on build
         webpackConfig.resolve.modules.push(path.resolve('../../node_modules'));
         webpackConfig.performance = {hints: false};
+
+        webpackConfig.plugins.push(
+            new webpack.ProvidePlugin({
+                Buffer: ['buffer', 'Buffer'],
+            }));
     },
 };
