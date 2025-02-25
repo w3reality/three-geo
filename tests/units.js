83f97d38a00b149e63f35e0a625c3c0b47a6353a
j-devel <38098238+j-devel@users.noreply.github.com>
2025-02-25 14:56:24 +0700

    Update units.js - `vec-pbf` call constructor

--- tests/units.js
+++ tests/units.js
@@ -120,9 +120,8 @@ units['vec-table'] = async (ThreeGeo, dataDir, preset='node') => {
 };
 
 units['vec-pbf'] = async (ThreeGeo, dataDir, preset='node') => {
-/*
     const tgeo = new ThreeGeo({ isDebug: true });
-
+/*
     const loc = {name: 'pbf', origin: [43.5, -79.5], radius: 1.25, zoom: 14}; // for '12-1143-1497.vector.pbf'
     setupApi(tgeo, preset, `${dataDir}/${loc.name}`);
 
